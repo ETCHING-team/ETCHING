6d14c58049f6f5ef28b0aa4cfa06674b8aa46f0e
sohnjangil <sohnjangil@gmail.com>
2021-09-15 15:49:57 +0900

    Update libconf.sh

--- libconf.sh
+++ libconf.sh
@@ -12,7 +12,7 @@ fi
 if (( version == 4 ))
 then
     subversion=$(g++ --version | head -n 1 | gawk '{print $3}' | gawk -F "." '{print $2}')
-    if [ $subversion < 7 ))
+    if (( $subversion < 7 ))
     then
 	echo "[Requirement))"
 	echo "You need gcc >4.7.0"
