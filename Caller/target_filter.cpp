0a7b527d1e1be0ddc4db7741ab0f3aa51a0d1001
Jang-il Sohn <sohnjang@gmail.com>
2021-09-15 10:08:14 +0900

    v1.3.6. Update for targeted panel sequencing

--- Caller/target_filter.cpp
+++ Caller/target_filter.cpp
@@ -7,9 +7,9 @@ std::size_t get_peak_memory();
 
 inline bool is_in_target ( std::string line , std::map < std::string , std::map < int , int > > & target_region ){
   std::stringstream ss ( line );
-  std::string chr1, chr2;
+  std::string chr1, chr2, tmp, alt, info;
   int pos1(-1), pos2(-1);
-  ss >> chr1 >> pos1 ;
+  ss >> chr1 >> pos1 >> tmp >> tmp >> alt >> tmp >> tmp >> info;
   if ( target_region.find ( chr1 ) != target_region.end() ){
     for ( auto i : target_region[chr1] ){
       if ( pos1 >= i.first && pos1 <= i.second ){
@@ -20,6 +20,53 @@ inline bool is_in_target ( std::string line , std::map < std::string , std::map 
 
   // search for chr2:pos2
 
+  std::stringstream info_ss ( info );
+  std::map < std::string , std::string > info_map;
+  std::string key;
+  std::string val;
+  
+  while ( std::getline ( info_ss , tmp , ';' ) ){
+    std::stringstream tmp_ss ( tmp );
+    std::getline ( tmp_ss , key , '=' );
+    std::getline ( tmp_ss , val , '=' );
+    info_map[key]=val;
+  }
+  
+  if ( info_map.find("END") != info_map.end () ){
+    pos2=atoi(info_map["END"].c_str());
+    if ( info_map.find("CHR2") != info_map.end () ){
+      chr2=info_map["CHR2"];
+    }
+    else{
+      chr2=chr1;
+    }
+  }
+  else{
+    std::size_t found1 = ( alt.find ( ":" ) != std::string::npos ) ;
+    std::size_t found2 = ( alt.find ( "[" ) != std::string::npos ) ;
+    std::size_t found3 = ( alt.find ( "]" ) != std::string::npos ) ;
+
+    if ( found1 && found2 && ! found3 ){
+      std::stringstream ss1(alt);
+      std::getline ( ss1 , tmp , '[' );      
+      std::getline ( ss1 , alt , '[' );      
+    }
+    else if ( found1 && ! found2 && found3 ){
+      std::stringstream ss1(alt);
+      std::getline ( ss1 , tmp , ']' );      
+      std::getline ( ss1 , alt , ']' );      
+    }
+    else return 0;
+
+    std::stringstream ss1(alt);
+    std::getline ( ss1 , chr2 , ':' );
+    std::getline ( ss1 , tmp , ':' );
+    pos2=atoi(tmp.c_str());
+  }
+
+
+  //////////////
+  /*
   std::size_t found = line.find ( "CHR2=" );
   if ( found != std::string::npos ){
     line = line.substr ( found + 5 );
@@ -34,8 +81,23 @@ inline bool is_in_target ( std::string line , std::map < std::string , std::map 
     }
   }
   else {
-    return 0;
+    found = alt.find ( ":" );
+    if ( found != std::string::npos ) {
+      if ( alt[0]==']' || alt[0]=='[' ){
+	alt = alt.substr(1,alt.size()-3);
+      }
+      else if ( alt[1]==']' || alt[1]=='[' ) {
+	alt = alt.substr(2,alt.size()-3);	
+      }
+      else return 0;
+    }
+    else return 0;
+    std::stringstream ss1(alt);
+    std::getline ( ss1 , chr2 , ':' );
+    std::getline ( ss1 , tmp , ':' );
+    pos2=atoi(tmp.c_str());
   }
+  */
 
   if ( target_region.find ( chr2 ) != target_region.end() ){
     for ( auto i : target_region[chr2] ){
@@ -81,10 +143,13 @@ int main ( int argc , char ** argv ){
   std::cerr << "[Reading vcf: " << infile_vcf << "]\n";
   fin.open ( infile_vcf );
   while ( std::getline ( fin , tmp ) ){
-    if ( tmp[0] == '#' ){
-      std::cout << tmp << "\n";
+    std::cout << tmp << "\n";
+    if ( tmp[1] != '#' ){
+      break;
     }
-    else if ( is_in_target ( tmp , target_region ) ) {
+  }
+  while ( std::getline ( fin , tmp ) ){
+    if ( is_in_target ( tmp , target_region ) ) {
       std::cout << tmp << "\n";
     }
   }
